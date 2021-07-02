97fc135f6b21b810ee6a6e6219cc7f04ee7932af
Harish Kuna <hakuna@3c22fbe1d295.ant.amazon.com>
2021-07-02 12:07:39 -0700

    add awslb addon with IRSA to testbed

--- testbed/addons.ts
+++ testbed/addons.ts
@@ -1,6 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import * as eks from '@aws-cdk/aws-eks';
 import { Flux, RepositoryProps } from './addons/flux/construct';
+import { AWSLoadBalancerController } from './addons/awslb/construct';
 
 export interface AddonsProps {
   cluster: eks.Cluster
@@ -14,6 +15,11 @@ export class Addons extends cdk.Construct {
     new Flux(this, 'Flux', {
       cluster: props.cluster,
       repositories: props.repositories,
+    });
+
+    new AWSLoadBalancerController(this, 'AWSLoadBalancerController', {
+      cluster: props.cluster,
+      namespace: 'kube-system'
     });
   }
 }
