d0d7279b26d501d3cbc1864865adca2e1d2399ba
hakuna-matatah <hakuna@amazon.com>
2021-07-08 17:16:28 -0700

    karpenter addon for testbed (#8)
    * testbed withflux orchestrating addons
    
    * stack instantiation
    
    * add awslb addon with IRSA to testbed
    
    * add karpenter to testbed
    
    Co-authored-by: Harish Kuna <hakuna@3c22fbe1d295.ant.amazon.com>

---
+++ testbed/addons/karpenter/construct.ts
@@ -0,0 +1,74 @@
+import cdk = require('@aws-cdk/core');
+import eks = require('@aws-cdk/aws-eks');
+import iam = require('@aws-cdk/aws-iam');
+
+export interface KarpenterProps {
+    cluster: eks.Cluster;
+    namespace?: string;
+}
+
+export class Karpenter extends cdk.Construct {
+    constructor(scope: cdk.Construct, id: string, props: KarpenterProps) {
+        super(scope, id);
+
+        const ns = props.cluster.addManifest('namespace', {
+            apiVersion: 'v1',
+            kind: 'Namespace',
+            metadata: {
+                name: props.namespace ?? "karpenter"
+            }
+        });
+
+        const sa = props.cluster.addServiceAccount('karpenter-controller-sa', {
+            name: "karpenter",
+            namespace: props.namespace ?? "karpenter"
+        });
+        sa.node.addDependency(ns);
+        sa.role.attachInlinePolicy(new iam.Policy(this, 'karpenter-controller-policy', {
+            statements: [
+                new iam.PolicyStatement({
+                    resources: ['*'],
+                    actions: ["ec2:CreateLaunchTemplate", "ec2:CreateFleet", "ec2:RunInstances",
+                        "ec2:CreateTags", "iam:PassRole", "ec2:TerminateInstances", "ec2:DescribeLaunchTemplates",
+                        "ec2:DescribeInstances", "ec2:DescribeSecurityGroups", "ec2:DescribeSubnets",
+                        "ec2:DescribeInstanceTypes", "ec2:DescribeInstanceTypeOfferings", "ec2:DescribeAvailabilityZones",
+                        "ssm:GetParameter"],
+                }),
+            ],
+        }));
+
+        props.cluster.awsAuth.addRoleMapping(new iam.Role(this, 'karpenter-node-role', {
+            assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEKSWorkerNodePolicy'),
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2ContainerRegistryReadOnly'),
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEKS_CNI_Policy'),
+                iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+            ]
+        }),
+            {
+                username: `system:node:{{EC2PrivateDNSName}}`,
+                groups: ['system:bootstrappers', 'system:nodes']
+            }
+        )
+
+        const chart = props.cluster.addHelmChart('KarpenterHelmChart', {
+            chart: 'karpenter',
+            release: 'karpenter',
+            version: 'v0.2.8',
+            repository: 'https://awslabs.github.io/karpenter/charts',
+            namespace: "karpenter",
+            createNamespace: false,
+            values: {
+                'serviceAccount': {
+                    'create': false,
+                    'name': sa.serviceAccountName,
+                    'annotations': {
+                        'eks.amazonaws.com/role-arn': sa.role.roleArn
+                    }
+                }
+            }
+        });
+        chart.node.addDependency(ns);
+    }
+}
