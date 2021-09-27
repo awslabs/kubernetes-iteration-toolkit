f83a9a858cd2b582073e3cca9a9b47be6e0ee545
hakuna-matatah <hakuna@amazon.com>
2021-09-27 09:07:33 -0700

    Testbed kit addon installation; sync tests; (#39)
    * testworklows sync using flux;add tekton triggers rbac;kit addon installation using flux;refacor;bug-fixes;update eksctl image.
    
    Co-authored-by: Harish Kuna <hakuna@3c22fbe1d295.ant.amazon.com>

--- testbed/bin/testbed.ts
+++ testbed/bin/testbed.ts
@@ -8,5 +8,10 @@ new Testbed(new cdk.App(), 'testbed', {
         region: process.env.CDK_DEFAULT_REGION
     },
     repositories: [
-        { name: "testbed", url: "https://github.com/awslabs/kubernetes-iteration-toolkit", path: "./testbed/addons" }]
+        { name: "testbed", url: "https://github.com/awslabs/kubernetes-iteration-toolkit", path: "./testbed/addons" },
+        { name: "tests", url: "https://github.com/awslabs/kubernetes-iteration-toolkit", path: "./tests" },
+        //To-do: move to a different repository
+        //{ name: "triggers", url: "https://github.com/awslabs/kubernetes-iteration-toolkit", path: "./tests/triggers" }
+
+        ]
 });
