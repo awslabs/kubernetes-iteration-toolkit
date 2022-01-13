65ee969a7b0a248735a86c86cda2d01f9a273e46
Ellis Tarn <ellistarn@gmail.com>
2022-01-13 08:56:57 -0800

    Moved from ASG to Fleet for faster devloop (#111)
    * Moved from ASG to Fleet for faster devloop
    
    * PR Comments
    
    * PR Commentsg

--- substrate/pkg/controller/substrate/cluster/instance.go
+++ substrate/pkg/controller/substrate/cluster/instance.go
@@ -20,8 +20,7 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/awslabs/kit/substrate/pkg/apis/v1alpha1"
 	"github.com/awslabs/kit/substrate/pkg/utils/discovery"
 	"knative.dev/pkg/logging"
@@ -29,59 +28,99 @@ import (
 )
 
 type Instance struct {
-	AutoScaling *autoscaling.AutoScaling
+	EC2         *ec2.EC2
 }
 
-func (a *Instance) Create(ctx context.Context, substrate *v1alpha1.Substrate) (reconcile.Result, error) {
-	if len(substrate.Status.PublicSubnetIDs) == 0 {
+func (i *Instance) Create(ctx context.Context, substrate *v1alpha1.Substrate) (reconcile.Result, error) {
+	if len(substrate.Status.PublicSubnetIDs) == 0 || substrate.Status.Cluster.LaunchTemplateVersion == nil {
 		return reconcile.Result{Requeue: true}, nil
 	}
-	if _, err := a.AutoScaling.CreateAutoScalingGroupWithContext(ctx, &autoscaling.CreateAutoScalingGroupInput{
-		AutoScalingGroupName: discovery.Name(substrate),
-		DesiredCapacity:      aws.Int64(1), MaxSize: aws.Int64(1), MinSize: aws.Int64(1),
-		LaunchTemplate:    &autoscaling.LaunchTemplateSpecification{LaunchTemplateName: discovery.Name(substrate), Version: aws.String("$Latest")},
-		VPCZoneIdentifier: aws.String(strings.Join(substrate.Status.PublicSubnetIDs, ",")),
-		Tags: []*autoscaling.Tag{{
-			Key:               aws.String(discovery.OwnerTagKey),
-			Value:             aws.String(substrate.Name),
-			PropagateAtLaunch: aws.Bool(true),
-		}, {
-			Key:               aws.String("Name"),
-			Value:             discovery.Name(substrate),
-			PropagateAtLaunch: aws.Bool(true),
-		}},
-	}); err != nil {
-		if strings.Contains(err.Error(), "Invalid IAM Instance Profile name") { // Instance Profile can take several seconds to propagate
+	instancesOutput, err := i.EC2.DescribeInstancesWithContext(ctx, &ec2.DescribeInstancesInput{Filters: discovery.Filters(substrate)})
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("describing instances, %w", err)
+	}
+	for _, reservation := range instancesOutput.Reservations {
+		for _, instance := range reservation.Instances {
+			if aws.StringValue(instance.State.Name) == ec2.InstanceStateNameRunning || aws.StringValue(instance.State.Name) == ec2.InstanceStateNamePending {
+				for _, tag := range instance.Tags {
+					if aws.StringValue(tag.Key) == "aws:ec2launchtemplate:version" && aws.StringValue(tag.Value) == aws.StringValue(substrate.Status.Cluster.LaunchTemplateVersion) {
+						logging.FromContext(ctx).Infof("Found instance %s", aws.StringValue(instance.InstanceId))
+						return reconcile.Result{}, nil
+					}
+				}
+			}
+		}
+	}
+	overrides := []*ec2.FleetLaunchTemplateOverridesRequest{}
+	for _, subnet := range substrate.Status.PublicSubnetIDs {
+		overrides = append(overrides, &ec2.FleetLaunchTemplateOverridesRequest{SubnetId: aws.String(subnet)})
+	}
+	createFleetOutput, err := i.EC2.CreateFleetWithContext(ctx, &ec2.CreateFleetInput{
+		Type: aws.String(ec2.FleetTypeInstant),
+		LaunchTemplateConfigs: []*ec2.FleetLaunchTemplateConfigRequest{{
+			Overrides: overrides,
+			LaunchTemplateSpecification: &ec2.FleetLaunchTemplateSpecificationRequest{
+				LaunchTemplateName: discovery.Name(substrate),
+				Version:            substrate.Status.Cluster.LaunchTemplateVersion,
+			}},
+		},
+		TargetCapacitySpecification: &ec2.TargetCapacitySpecificationRequest{
+			DefaultTargetCapacityType: aws.String(ec2.DefaultTargetCapacityTypeOnDemand),
+			TotalTargetCapacity:       aws.Int64(1),
+		},
+		TagSpecifications: discovery.Tags(substrate, ec2.ResourceTypeInstance, discovery.Name(substrate)),
+		OnDemandOptions:   &ec2.OnDemandOptionsRequest{AllocationStrategy: aws.String(ec2.FleetOnDemandAllocationStrategyLowestPrice)},
+	})
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("creating fleet, %w", err)
+	}
+	for _, err := range createFleetOutput.Errors {
+		if strings.Contains(aws.StringValue(err.ErrorMessage), "Invalid IAM Instance Profile name") {
 			return reconcile.Result{Requeue: true}, nil
 		}
-		if err.(awserr.Error).Code() != autoscaling.ErrCodeAlreadyExistsFault {
-			return reconcile.Result{}, fmt.Errorf("creating autoscaling group, %w", err)
+		return reconcile.Result{}, fmt.Errorf("creating fleet", aws.StringValue(err.ErrorMessage))
+	}
+	logging.FromContext(ctx).Infof("Created instance %s", aws.StringValue(createFleetOutput.Instances[0].InstanceIds[0]))
+
+	if err := i.delete(ctx, substrate, func(instance *ec2.Instance) bool {
+		if aws.StringValue(instance.InstanceId) == aws.StringValue(createFleetOutput.Instances[0].InstanceIds[0]) {
+			return false
 		}
-		logging.FromContext(ctx).Infof("Found auto scaling group %s", aws.StringValue(discovery.Name(substrate)))
-	} else {
-		logging.FromContext(ctx).Infof("Created auto scaling group %s", aws.StringValue(discovery.Name(substrate)))
+		return aws.StringValue(instance.State.Name) == ec2.InstanceStateNameRunning ||
+			aws.StringValue(instance.State.Name) == ec2.InstanceStateNamePending
+	}); err != nil {
+		return reconcile.Result{}, err
 	}
 	return reconcile.Result{}, nil
 }
 
-func (a *Instance) Delete(ctx context.Context, substrate *v1alpha1.Substrate) (reconcile.Result, error) {
-	autoscalingGroupsOutput, err := a.AutoScaling.DescribeAutoScalingGroupsWithContext(ctx, &autoscaling.DescribeAutoScalingGroupsInput{
-		AutoScalingGroupNames: []*string{discovery.Name(substrate)},
+func (i *Instance) Delete(ctx context.Context, substrate *v1alpha1.Substrate) (reconcile.Result, error) {
+	return reconcile.Result{}, i.delete(ctx, substrate, func(instance *ec2.Instance) bool {
+		return aws.StringValue(instance.State.Name) != ec2.InstanceStateNameShuttingDown &&
+			aws.StringValue(instance.State.Name) != ec2.InstanceStateNameTerminated
 	})
+}
+
+func (i *Instance) delete(ctx context.Context, substrate *v1alpha1.Substrate, predicate func(*ec2.Instance) bool) error {
+	instancesOutput, err := i.EC2.DescribeInstancesWithContext(ctx, &ec2.DescribeInstancesInput{Filters: discovery.Filters(substrate)})
 	if err != nil {
-		return reconcile.Result{}, fmt.Errorf("describing auto scaling groups, %w", err)
+		return fmt.Errorf("describing instances, %w", err)
 	}
-	for _, group := range autoscalingGroupsOutput.AutoScalingGroups {
-		if aws.StringValue(group.Status) == "Delete in progress" {
-			continue
+	instances := []*string{}
+	for _, reservation := range instancesOutput.Reservations {
+		for _, instance := range reservation.Instances {
+			if predicate(instance) {
+				instances = append(instances, instance.InstanceId)
+
+			}
 		}
-		if _, err := a.AutoScaling.DeleteAutoScalingGroupWithContext(ctx, &autoscaling.DeleteAutoScalingGroupInput{
-			AutoScalingGroupName: group.AutoScalingGroupName,
-			ForceDelete:          aws.Bool(true),
-		}); err != nil {
-			return reconcile.Result{}, fmt.Errorf("deleting autoscaling group, %w", err)
-		}
-		logging.FromContext(ctx).Infof("Deleted auto scaling group %s", aws.StringValue(discovery.Name(substrate)))
 	}
-	return reconcile.Result{}, nil
+	if len(instances) == 0 {
+		return nil
+	}
+	if _, err := i.EC2.TerminateInstancesWithContext(ctx, &ec2.TerminateInstancesInput{InstanceIds: instances}); err != nil {
+		return fmt.Errorf("terminating instances, %w", err)
+	}
+	logging.FromContext(ctx).Infof("Deleted instances %v", aws.StringValueSlice(instances))
+	return nil
 }
