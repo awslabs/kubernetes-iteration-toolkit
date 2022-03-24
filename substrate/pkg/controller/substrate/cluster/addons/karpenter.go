3ed1bbd47f7b8f111208e977acaa3edfa1834ca8
Felix Zhe Huang <92051185+felix-zhe-huang@users.noreply.github.com>
2022-03-24 14:18:41 -0400

    Update substrate component versions (#164)
    * Update substrate component versions
    
    * Add the missing repo URL

--- substrate/pkg/controller/substrate/cluster/addons/karpenter.go
+++ substrate/pkg/controller/substrate/cluster/addons/karpenter.go
@@ -66,7 +66,7 @@ func (k *Karpenter) Create(ctx context.Context, substrate *v1alpha1.Substrate) (
 		Namespace:       "karpenter",
 		Name:            "karpenter",
 		Repository:      "https://charts.karpenter.sh",
-		Version:         "0.7.1",
+		Version:         "0.7.3",
 		CreateNamespace: true,
 		Values: map[string]interface{}{
 			"clusterName":     substrate.Name,
