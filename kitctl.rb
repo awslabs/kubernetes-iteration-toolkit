3c1ae32ce80721b3b7df6f709e05a3a71b502d95
Prateek Gogia <prateekgogia42@gmail.com>
2022-09-14 23:06:54 -0400

    Update kitctl to v0.0.20 Created by Github actions during Release CLI action

--- kitctl.rb
+++ kitctl.rb
@@ -2,10 +2,10 @@
 require 'formula'
 class Kitctl < Formula
   homepage 'https://github.com/awslabs/kubernetes-iteration-toolkit/substrate'
-  version '0.0.19'
+  version '0.0.20'
   if OS.mac? && Hardware::CPU.is_64_bit?
-    url 'https://github.com/awslabs/kubernetes-iteration-toolkit/releases/download/v0.0.19/kitctl_v0.0.19_darwin_amd64.zip'
-    sha256 'cccf5230d802c00b36d1214e5f779e250d727c543cbfeb5b0614c313a6a20ca5'
+    url 'https://github.com/awslabs/kubernetes-iteration-toolkit/releases/download/v0.0.20/kitctl_v0.0.20_darwin_amd64.zip'
+    sha256 'ca1a31e1f1b8968d595c5f3d4386c5b7e32a76e5efecb3224217e907136fae73'
   else
     echo "Hardware not supported"
     exit 1
