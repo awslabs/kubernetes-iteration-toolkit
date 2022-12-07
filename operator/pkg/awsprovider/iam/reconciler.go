a6763db526664d3d509cb75914d4059b91b8daff
Mengqi (David) Yu <mengqiyu@amazon.com>
2022-12-07 20:33:57 +0000

    grant AmazonPrometheusRemoteWriteAccess in kit operator

--- operator/pkg/awsprovider/iam/reconciler.go
+++ operator/pkg/awsprovider/iam/reconciler.go
@@ -45,6 +45,7 @@ var (
 		"arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
 		"arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
 		"arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+		"arn:aws:iam::aws:policy/AmazonPrometheusRemoteWriteAccess",
 	}
 )
 
