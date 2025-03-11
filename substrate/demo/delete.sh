2bcbbf96e2f7251a5109a73f36c828adf1ed20f1
Cameron Rozean <rcrozean@amazon.com>
2025-03-11 11:54:06 -0700

    remove unused workflows and code for substrate and operator (#494)

--- substrate/demo/delete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-
-# Delete Cluster
-clusterctl move --kubeconfig ./substrate.kubeconfig --to-kubeconfig ./bootstrap.kubeconfig
-kubectl delete --kubeconfig bootstrap.kubeconfig -f ./substrate.yaml
-kind delete cluster --name bootstrap
